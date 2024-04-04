ef121bd82d2b91f407cf886d6e61dbd5de3c874f
Laxminarayan-Birle <laxminarayan.2022ca046@mnnit.ac.in>
2024-04-04 20:15:38 +0530

    init abc.cpp

---
+++ abc.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/bitwise-ors-of-subarrays/
+class Solution
+{
+public:
+    int subarrayBitwiseORs(vector<int> &arr)
+    {
+        int i;
+        int j, n = arr.size();
+        unordered_set<int> st;
+        for (i = 0; i < n; i++)
+        {
+
+            int pre = 0, curr = arr[i];
+            st.insert(arr[i]);
+            for (j = i + 1; j < n; j++)
+            {
+
+                curr = (curr | arr[j]);
+                pre = pre | arr[j];
+                st.insert(curr);
+                if (pre == curr)
+                    break;
+            }
+        }
+        return st.size();
+    }
+};
